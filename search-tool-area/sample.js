05ff32bd62574a404c7b4ba58cbe53a30a756db1
Riku <62926328+RikuWorks@users.noreply.github.com>
2021-01-21 14:26:02 +0900

    Add files via upload

---
+++ search-tool-area/sample.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('input#search1').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search2').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search3').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search4').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search5').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search6').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search7').quicksearch('table tbody tr');
+});
+$(function(){
+  $('input#search8').quicksearch('table tbody tr');
+});
